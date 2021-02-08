ab0814c2953d7bbcf51bb06b706e0a9e9ec13dcd
Dina Masri <72582239+dinasmasri@users.noreply.github.com>
2021-02-07 16:00:09 -0800

    Add files via upload

---
+++ FramerJSComponents.tsx
@@ -0,0 +1,253 @@
+
+<Tabs
+  activeColor={"hsl(0, 0%, 100%)"}
+  background={"hsl(2, 28%, 65%)"}
+  children={[...]}
+  defaultTab={1}
+  tab1Icon={"Home"}
+  tab2Icon={"Search"}
+  tab3Icon={"Plus"}
+  tab4Icon={"User"}
+  tab5Icon={"User"}
+  {/* Using default values */}
+  barHeight={75}
+  contentHeight={"stretch"}
+  contentWidth={"stretch"}
+  distribution={"space-around"}
+  gap={10}
+  iconSize={25}
+  inactiveColor={"#e1e1e1"}
+  padding={0}
+/
+
+
+<DataComponent
+  airtableUrl={"https://api.airtable.com/v0/appVkLSZDYjzM7Yar/Imported%20table?api_key=keyohA6yKk8P9I9q5"}
+  columns={2}
+  dataSource={"airtable"}
+  emptyState={Empty State}
+  horizontalGap={20}
+  listItem={Card}
+  loadingState={Loading State}
+  onItemTap={Interaction}
+  padding={5}
+  verticalGap={12}
+  {/* Using default values */}
+  airtableImageSize={"large"}
+  apiResponseDataKey={"data"}
+  apiUrl={"https://reqres.in/api/users?page=1"}
+  dataSourceFileType={"json"}
+  direction={"vertical"}
+  gap={0}
+  httpAuthorizationHeader={""}
+  httpHeaders={["X-API-Key: 14a96448-1ae1-4de3-94c7-6f6e828e4519"]}
+  isDragScrollEnabled={true}
+  isScrollEnabled={true}
+  isSearchEnabled={false}
+  isWheelScrollEnabled={true}
+  loadingDelay={0}
+  mode={"default"}
+  overrideHttpHeaders={false}
+  searchTerm={""}
+  shouldSort={false}
+  sortDirection={"ascending"}
+  sortKey={"price"}
+  verticalAlignment={"flex-start"}
+  verticalDistribution={"space-around"}
+  wrap={"nowrap"}
+/>
+
+<Select
+  defaultBackgroundColor={"rgb(247, 218, 217)"}
+  defaultBorderColor={"rgb(150, 140, 131)"}
+  defaultBorderWidth={2}
+  options={["All", "Tops", "Bottoms", "Hoodies/Sweatshirts", "Baby", "Shoes", "Outerwear"]}
+  {/* Using default values */}
+  defaultIconColor={"#999999"}
+  defaultTextColor={"#444444"}
+  disabled={false}
+  disabledBackgroundColor={"#f2f2f2"}
+  disabledBorderColor={"#09F"}
+  disabledBorderWidth={0}
+  disabledIconColor={"#999999"}
+  disabledOpacity={60}
+  disabledTextColor={"#444444"}
+  focusBackgroundColor={"#f2f2f2"}
+  focusBorderColor={"#09F"}
+  focusBorderWidth={1}
+  focusIconColor={"#999999"}
+  focusTextColor={"#444444"}
+  focused={false}
+  font={false}
+  fontFamily={""}
+  fontSize={16}
+  radius={8}
+  value={1}
+  variant={"default"}
+/>
+
+<Camera
+  facingMode={"user"}
+  {/* Using default values */}
+/>
+
+<DataComponent
+  airtableUrl={"https://api.airtable.com/v0/appzJay8xpC9oh1xV/Imported%20table?api_key=keyohA6yKk8P9I9q5"}
+  columns={2}
+  dataSource={"airtable"}
+  horizontalGap={12}
+  listItem={Card - ML}
+  onItemTap={Interaction}
+  padding={5}
+  shouldSort={true}
+  sortKey={"label"}
+  verticalGap={8}
+  {/* Using default values */}
+  airtableImageSize={"large"}
+  apiResponseDataKey={"data"}
+  apiUrl={"https://reqres.in/api/users?page=1"}
+  dataSourceFileType={"json"}
+  direction={"vertical"}
+  gap={0}
+  httpAuthorizationHeader={""}
+  httpHeaders={["X-API-Key: 14a96448-1ae1-4de3-94c7-6f6e828e4519"]}
+  isDragScrollEnabled={true}
+  isScrollEnabled={true}
+  isSearchEnabled={false}
+  isWheelScrollEnabled={true}
+  loadingDelay={0}
+  mode={"default"}
+  overrideHttpHeaders={false}
+  searchTerm={""}
+  sortDirection={"ascending"}
+  verticalAlignment={"flex-start"}
+  verticalDistribution={"space-around"}
+  wrap={"nowrap"}
+/>
+
+// On Item Tap - Navigation
+type: "spring",
+delay: 0,
+stiffness: 500,
+damping: 60,
+mass: 1,
+
+
+<DataComponent
+  airtableUrl={"https://api.airtable.com/v0/applBBe6so30Xyere/Imported%20table?api_key=keyohA6yKk8P9I9q5"}
+  columns={2}
+  dataSource={"airtable"}
+  horizontalGap={12}
+  listItem={Card - saved Items}
+  onItemTap={Interaction}
+  padding={5}
+  shouldSort={true}
+  sortKey={"label"}
+  verticalGap={8}
+  {/* Using default values */}
+  airtableImageSize={"large"}
+  apiResponseDataKey={"data"}
+  apiUrl={"https://reqres.in/api/users?page=1"}
+  dataSourceFileType={"json"}
+  direction={"vertical"}
+  gap={0}
+  httpAuthorizationHeader={""}
+  httpHeaders={["X-API-Key: 14a96448-1ae1-4de3-94c7-6f6e828e4519"]}
+  isDragScrollEnabled={true}
+  isScrollEnabled={true}
+  isSearchEnabled={false}
+  isWheelScrollEnabled={true}
+  loadingDelay={0}
+  mode={"default"}
+  overrideHttpHeaders={false}
+  searchTerm={""}
+  sortDirection={"ascending"}
+  verticalAlignment={"flex-start"}
+  verticalDistribution={"space-around"}
+  wrap={"nowrap"}
+/>
+
+// On Item Tap - Navigation
+type: "spring",
+delay: 0,
+stiffness: 500,
+damping: 60,
+mass: 1,
+
+
+
+
+// On Item Tap - Navigation
+type: "spring",
+delay: 0,
+stiffness: 500,
+damping: 60,
+mass: 1,
+
+
+<Input
+  backgroundColor={"hsl(0, 0%, 100%)"}
+  border={"var(--medium_pink, rgb(240, 180, 178))"}
+  borderWidth={3}
+  focusColor={"#10A775"}
+  font={true}
+  padding={12}
+  placeholder={"Enter a clothing Item"}
+  placeholderColor={"hsl(28, 6%, 45%)"}
+  radius={12}
+  textColor={"hsl(0, 0%, 0%)"}
+  {/* Using default values */}
+  focused={false}
+  fontFamily={""}
+  fontSize={16}
+  keyboard={""}
+  multiLine={false}
+  password={false}
+  value={""}
+/>
+
+<DataComponent
+  airtableUrl={"https://api.airtable.com/v0/appVkLSZDYjzM7Yar/Imported%20table?api_key=keyohA6yKk8P9I9q5"}
+  apiUrl={"https://api.airtable.com/v0/appVkLSZDYjzM7Yar/Imported%20table?api_key=keyohA6yKk8P9I9q5"}
+  csvFileUrl={data_with_description.csv}
+  dataSource={"airtable"}
+  dataSourceFileType={"csv"}
+  emptyState={Empty State}
+  gap={24}
+  isSearchEnabled={true}
+  listItem={List Item With Price}
+  loadingState={Loading State}
+  onItemTap={Interaction}
+  shouldSort={true}
+  sortKey={"label"}
+  verticalDistribution={"flex-start"}
+  {/* Using default values */}
+  airtableImageSize={"large"}
+  apiResponseDataKey={"data"}
+  columns={1}
+  direction={"vertical"}
+  horizontalGap={0}
+  httpAuthorizationHeader={""}
+  httpHeaders={["X-API-Key: 14a96448-1ae1-4de3-94c7-6f6e828e4519"]}
+  isDragScrollEnabled={true}
+  isScrollEnabled={true}
+  isWheelScrollEnabled={true}
+  loadingDelay={0}
+  mode={"default"}
+  overrideHttpHeaders={false}
+  padding={0}
+  searchTerm={""}
+  sortDirection={"ascending"}
+  verticalAlignment={"flex-start"}
+  verticalGap={0}
+  wrap={"nowrap"}
+/>
+
+// On Item Tap - Navigation
+type: "spring",
+delay: 0,
+stiffness: 500,
+damping: 60,
+mass: 1,
+
+
